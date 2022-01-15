e3baca897c6850fbd33f9eb232a0ec9421d8b9de
huyq78 <huyq78@users.noreply.github.com>
2022-01-15 03:00:30 +0000

    deploy: 38450f390e8ac72ea3d766916e4678813fb59361

---
+++ search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['gtype_5fcmp_5fs_0',['gtype_cmp_s',['../uniongeneric__type.html#a46c338d7295e88dce828ae386da16140',1,'generic_type']]],
+  ['gvec_5fcreate_1',['gvec_create',['../structgvec__s.html#a71fb4b23fba82d78ab64cc2e5ef534e5',1,'gvec_s']]]
+];
